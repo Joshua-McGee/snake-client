3b3e30de7dcdd14fb361dada7231abd6f69ae380
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-31 20:55:03 +0000

    moved most the code in play to client

--- play.js
+++ play.js
@@ -3,33 +3,28 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.88.218',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: '192.168.88.218',
+//     port: 50541
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); 
 
-  // event handler to handle incoming data and console log it for the player.
-  conn.on('data', data => {
-        //conn.end();
-        //process.exit();   
+//   // event handler to handle incoming data and console log it for the player.
+//   conn.on('data', data => {
+//         //conn.end();
+//         //process.exit();   
 
-    console.log('data', data)
-
-    //conn.write(data) if writing data it will create a loop and crash
-})
-
-// conn.on('connect', () => {
-//   console.log('I connected!')
-//   //conn.write('Hello world!')
+//     console.log('data', data)
 // })
 
-  return conn;
-}
+//   return conn;
+// }
 
+// console.log('Connecting ...');
+// connect();
 
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
