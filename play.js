cb3edf43f5c3a899662794461c590387536a3bc1
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-31 22:41:06 +0000

    added a constants.js file to hold some global variables, also updated play.js to be easier to read

--- play.js
+++ play.js
@@ -1,27 +1,3 @@
-const net = require('net');
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-// const setupInput = function() {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding('utf8');
-//   stdin.resume();
-
-//   // uses a callback to kill the process
-//   process.stdin.on('data', handleUserInput);
- 
-//   return stdin;
-// }
-
-// // a function that checks if you have hit control + c to exit the line
-// const handleUserInput = function(stdin) {
-//   if (stdin === '\u0003'){
-//     process.exit();
-//   }
-// };
 const { setupInput } = require('./input');
 const { connect } = require('./client');
 
